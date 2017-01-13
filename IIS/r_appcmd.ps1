e4ac3c87efdeafe4a8eb20f8a26eedf42290d0f0
TedZhang <Ted.Zhanghl@ef.com>
2017-01-13 15:17:23 +0800

    remote cmd

---
+++ IIS/r_appcmd.ps1
@@ -0,0 +1,17 @@
+﻿param(
+  [string] $servers,#split with '::'
+  [string] $parameter
+)
+
+$appcmd="C:\Windows\System32\inetsrv\appcmd.exe"
+$script = $appcmd + " " + $parameter
+
+$scriptBlock = [Scriptblock]::Create($script)
+
+
+ForEach($server in $servers.Split("::", [System.StringSplitOptions]::RemoveEmptyEntries))
+{
+    Write-Host
+    Write-Host "[$server]  $script"
+	Invoke-Command -ComputerName $server -ScriptBlock $scriptBlock
+}
---
+++ remote.ps1
@@ -0,0 +1,15 @@
+﻿param(
+  [string] $servers,
+  [string] $script
+)
+
+
+$scriptBlock = [Scriptblock]::Create($script)
+
+
+ForEach($server in $servers.Split("::", [System.StringSplitOptions]::RemoveEmptyEntries))
+{
+    Write-Host
+    Write-Host "[$server]  $script"
+	Invoke-Command -ComputerName $server -ScriptBlock $scriptBlock
+}
